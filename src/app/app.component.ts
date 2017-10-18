110c8141b7ddc4961a8eee8d67ff33a7b6689ff1
bebetoh <bebetohlf@gmail.com>
2017-10-18 00:26:57 -0200

    parei em fundamentals / diaplaying /Conditional display with NgIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero/hero';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'my-app',
+  templateUrl: '/app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  
+  title: string;
+  myHero: Hero;
+  heroes: Hero[];
+
+  constructor() {
+    this.title = 'Tour of Heroes';
+    //this.myHero = 'Windstorm';
+    this.heroes = [new Hero(1, 'Windstorm'),
+                  new Hero(13, 'Bombasto'),
+                  new Hero(15, 'Magneta'),
+                  new Hero(20, 'Tornado'),
+                  new Hero(25, 'Teste')];
+    this.myHero = this.heroes[1];
+  }
 }
