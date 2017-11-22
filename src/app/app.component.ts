68c5afe0f355c59a194299abb5349f3440889ed5
bebetoh <bebetohlf@gmail.com>
2017-11-21 23:47:45 -0200

    https://angular.io/guide/template-syntax#ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,10 +18,10 @@ export class AppComponent {
     this.title = 'Tour of Heroes';
     //this.myHero = 'Windstorm';
     this.heroes = [new Hero(1, 'Windstorm'),
-                  new Hero(13, 'Bombasto'),
-                  new Hero(15, 'Magneta'),
-                  new Hero(20, 'Tornado'),
-                  new Hero(25, 'Teste')];
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado'),
+    new Hero(25, 'Teste')];
     this.myHero = this.heroes[1];
     this.isUnchanged = false;
   }
@@ -33,17 +33,34 @@ export class AppComponent {
   testex = 20;
 
 
- isSpecial = true;
- canSave = true;
+  isSpecial = true;
+  canSave = true;
 
 
   currentClasses: {};
-setCurrentClasses() {
-  // CSS classes: added/removed per current state of component properties
-  this.currentClasses =  {
-    'saveable': this.canSave,
-    'modified': !this.isUnchanged,
-    'special' :  this.isSpecial
-  };
-}
+  setCurrentClasses() {
+    // CSS classes: added/removed per current state of component properties
+    this.currentClasses =  {
+      'saveable': this.canSave,
+      'modified': !this.isUnchanged,
+      'special' :  this.isSpecial
+    };
+  }
+
+  currentStyles: {};
+  setCurrentStyles() {
+    // CSS styles: set per current state of component properties
+    this.currentStyles = {
+      'font-style':  this.canSave      ? 'italic' : 'normal',
+      'font-weight': !this.isUnchanged ? 'bold'   : 'normal',
+      'font-size':   this.isSpecial    ? '24px'   : '12px'
+    };
+  }
+
+  currentHero= new Hero(20, '');
+
+  setUppercaseName(name: string) {
+      this.currentHero.name = name.toUpperCase();
+  }
+
 }
